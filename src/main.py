0a32519d113d56be26018dd323f76d41ad61eb2d
Siddhardhan <42094934+siddhardhan23@users.noreply.github.com>
2024-05-17 15:34:46 +0530

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,55 @@
+import os
+import json
+
+import streamlit as st
+import openai
+
+# configuring openai - api key
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_data = json.load(open(f"{working_dir}/config.json"))
+OPENAI_API_KEY = config_data["OPENAI_API_KEY"]
+openai.api_key = OPENAI_API_KEY
+
+# configuring streamlit page settings
+st.set_page_config(
+    page_title="GPT-4o Chat",
+    page_icon="💬",
+    layout="centered"
+)
+
+# initialize chat session in streamlit if not already present
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# streamlit page title
+st.title("🤖 GPT-4o - ChatBot")
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# input field for user's message
+user_prompt = st.chat_input("Ask GPT-4o...")
+
+if user_prompt:
+    # add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    # send user's message to GPT-4o and get a response
+    response = openai.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            *st.session_state.chat_history
+        ]
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display GPT-4o's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
